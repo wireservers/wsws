3f2f1aae734053d5b85b9d0de23c64b84599a602
Todd-WS <todd@wireservers.com>
2026-05-15 16:19:25 -0600

    update: mark react-natives subproject as dirty

--- security/packages/auth/src/core/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './types.js';
-export * from './authority.js';
-export * from './claims.js';
--- security/packages/auth/src/node/index.ts
+++
@@ -1,4 +0,0 @@
-export { createTokenValidator } from './validateToken.js';
-export type { ValidatedToken, TokenValidatorOptions } from './validateToken.js';
-export { authMiddleware } from './expressMiddleware.js';
-export type { AuthAppConfig, TenancyMode } from '../core/types.js';
